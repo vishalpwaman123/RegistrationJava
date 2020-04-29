48190cf4b035605653c736d8ac13c60e5b9bd4c0
vishalpwaman <vishalpwaman1997@gmail.com>
2020-04-29 07:45:34 -0400

    UseCase 3

--- UserMethodTesting.java
+++ UserMethodTesting.java
@@ -3,7 +3,13 @@ package com.CaseStudy;
 import java.net.PasswordAuthentication;
 import java.util.regex.Pattern;
 
-public String validateFirstName(String fname) {
+public class UserMethodTesting {
+
+    private static final String PATTERN = "^[A-Z]{1}[a-z]{3,}$";
+    private static final String EMAIL_PATTERN = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,3})?$";
+    
+
+    public String validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(PATTERN);
         if( pattern.matcher(fname).matches() == true)
         {
@@ -13,9 +19,11 @@ public String validateFirstName(String fname) {
             return "False";
         }
     }
-    
-    	public String validateLastName(String lname) {
+
+    public String validateLastName(String lname) {
         Pattern pattern = Pattern.compile(PATTERN);
+
+
         if( pattern.matcher(lname).matches() == true)
         {
             return "True";
@@ -24,6 +32,19 @@ public String validateFirstName(String fname) {
             return "False";
         }
     }
+
+    public String validateEmailAddress(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        
+        if( pattern.matcher(email).matches() == true)
+        {
+            return "True";
+        }else
+        {
+            return "False";
+        }
+    }
+    
 }
 
 
